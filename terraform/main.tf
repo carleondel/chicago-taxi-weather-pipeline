1a8077e2f9af9fa813d624af556515e4ad97f1fd
carleondel <carleondel@gmail.com>
2025-07-01 19:00:05 +0200

    Initial Terraform setup

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_storage_bucket" "weather_raw" {
+  name     = "${var.project_id}-weather-raw"
+  location = var.region
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "project_id" {
+  description = "Your GCP project ID"
+}
+
+variable "region" {
+  description = "GCP region"
+  default     = "us-central1"
+}
